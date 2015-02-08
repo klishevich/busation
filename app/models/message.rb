3a979f43b8e7dbaad1757cb959077969813255a0
klisha <mans1@ngs.ru>
2015-02-08 18:15:16 +0300

    antispam

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,7 +5,16 @@ class Message < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
 
+  validate :check_spam
+
   def deliver
     MessageMailer.new_notification(self).deliver
   end
+
+  def check_spam
+    if body.include? "href"
+      errors.add(:body, :spam)
+    end
+  end
+
 end
