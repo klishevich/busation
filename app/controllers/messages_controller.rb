3f707876eb6298ba77903a488adced6de31c2931
klishevich <busation@gmail.com>
2016-07-09 23:22:41 +0300

    mahs update and minor fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,35 +9,22 @@ class MessagesController < ApplicationController
   end
 
   def create
-    # mp = message_params
-    # mp[:mtype] = params[:type]
     @message = Message.new(message_params)
     if @message.save
       @message.deliver
-      # flash[:success] = t(:ok)
-      redirect_to @message
+      redirect_to '/ok'
     else
       render 'new'
     end
   end
 
-  def show
-    @message = Message.find(params[:id])
-  end
-
-  # def edit
-  # 	@message=Message.find(params[:id])
+  # def show
+  #   @message = Message.find(params[:id])
   # end
 
-  # def update
-  #   @message = Demand.find(params[:id])
-  #   if @demand.update_attributes(demand_params)
-  #     flash[:success] = t(:ok)
-  #     redirect_to @demand
-  #   else
-  #     render 'edit'
-  #   end
-  # end	
+  def ok
+    @message = Message.last
+  end
 
   private
 
